012256136c7c9d1cc6b0cea30ab74e55796a45ca
yiny <174821300@qq.com>
2021-09-03 13:24:38 +0800

    20210903-132438

---
+++ vue02_component/School.vue
@@ -0,0 +1,39 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学校名称{{name}}</h2>
+        <h2>学校地址{{address}}</h2>
+        <button @click="showName">点我提示名字</button>
+    </div>
+</template>
+
+<script>
+    // 组件交互相关的代码（数据、方法等等）
+    /*
+        完全写法：
+        const school = Vue.extend({...})
+        export default school
+    */
+    // 简写方式：
+    export default {
+        name: 'School',
+        data(){
+            return {
+                name: '极客堂',
+                address: '北京'
+            }
+        },
+        methods: {
+            showName(){
+                console.log(this.name)
+            }
+        },
+    }
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo {        
+        background-color: aqua 
+    }
+</style>
---
+++ vue02_component/Student.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="demo">
+        <h2>学生名称{{name}}</h2>
+        <h2>学生年龄{{age}}</h2>
+        <button @click="showName">点我提示名字</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Student',
+        data(){
+            return {
+                name: '宋小宝',
+                age: 30
+            }
+        },
+        methods: {
+            showName(){
+                console.log(this.name)
+            }
+        },
+    }
+</script>
+
+<style>
+    .demo {        
+        background-color: azure 
+    }
+</style>
